ba1242c5b6d75edd163a9648dfe5b10109dc9d18
daemon-wars <1035605+warren-peace@users.noreply.github.com>
2022-11-08 23:43:25 +0800

    description

---
+++ selectJoint.py
@@ -0,0 +1,17 @@
+'''
+The program:
+	1. Highlights the missing joints in the REFERENCE
+	2. Highlights the extra joints in the second skeleton
+	3. prints the path of the highlights in the console
+
+INSTRUCTIONS:
+	Select the ROOT joint of the REFERENCE first
+	then select the ROOT joint of the second skeleton. 
+	Run script
+
+Color Codes:
+	RED: Joints in the REFERENCE missing in the second skeleton
+	GREEN: Joints in the REFERENCE that might be misplaced in the second skeleton
+	PINK: Joints that may be misplaced based on REFERENCE
+	BLUE: Extra joints in the second skeleton
+'''
