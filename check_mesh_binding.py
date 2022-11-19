495277faf3718f80a0a649472b11d7e72fa303dc
daemon-wars <1035605+warren-peace@users.noreply.github.com>
2022-11-19 22:50:50 +0800

    test selection

--- check_mesh_binding.py
+++ check_mesh_binding.py
@@ -1,6 +1,17 @@
 '''
     Checks if a mesh has joints belonging to different skeletons (root joints). Unreal considers FBX skinned to different skeletons as invalid.
+
+    Instructions:
+    1. Select the mesh.
+    2. Run the script.
 '''
 
 import maya.cmds as mc
 
+# get selection
+sel = mc.ls(sl=True)
+
+joints = mc.skinCluster(sel, inf = True, q = True)
+
+for item in joints:
+    print(item)
