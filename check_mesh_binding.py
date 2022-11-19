40cf7acd63f87b728a3675dc3587a41e11bc4144
daemon-wars <1035605+warren-peace@users.noreply.github.com>
2022-11-19 23:19:46 +0800

    list root of found joints

--- check_mesh_binding.py
+++ check_mesh_binding.py
@@ -11,7 +11,14 @@ import maya.cmds as mc
 # get selection
 sel = mc.ls(sl=True)
 
+# get list of joints the mesh is bound to
 joints = mc.skinCluster(sel, inf = True, q = True)
 
+roots_list = []
+# get the root of each joint
 for item in joints:
-    print(item)
+    # root = item.split('|')[0]
+    # print(root)
+    # get full path for the joint and find its root
+    sel  = mc.ls(item, l = True)[0].split('|')[1]
+    print(sel)
